1a1d47bbc77ae01cc7e82e9cf325580223453a5e
kashkhan <kashkhan@gmail.com>
2021-08-10 21:19:39 -0600

    get

--- basic/main.py
+++ basic/main.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, request
+import json
+import time
+
 
 app = Flask(__name__)
 
@@ -6,6 +9,22 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+
+@app.route('/upload', methods=[ 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/uploads/uploaded_file.txt') 
+
+
+@app.route('/gettest/<path:subpath>')
+def gettest(subpath):
+    out = {"subpath": subpath }
+    return json.dumps(out, ensure_ascii = False)
+
+
 
 if __name__ == '__main__':
     app.run()
